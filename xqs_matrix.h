e811990389d377f79ff7cb6d728f9d7d6777d8d8
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2026-01-02 15:53:27 +0200

    wip

--- xqs_matrix.h
+++ xqs_matrix.h
@@ -1148,6 +1148,7 @@ public:
 
   void swap(xqs_matrix& other) noexcept
   {
+    m_allocator.swap(other.m_allocator);
     std::swap(m_capacity, other.m_capacity);
     std::swap(m_data, other.m_data);
     std::swap(m_row_count, other.m_row_count);
