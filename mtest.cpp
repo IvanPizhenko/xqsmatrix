fd5ded370f6e0327c3bd86654e171181c75ab756
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2026-02-06 19:05:30 +0200

    Rename header file

--- mtest.cpp
+++ mtest.cpp
@@ -20,7 +20,7 @@
 // THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ///////////////////////////////////////////////////////////////////////////////
 
-#include "xqs_matrix.h"
+#include "matrix.h"
 
 #include <iostream>
 #include <random>
