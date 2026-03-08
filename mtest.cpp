40139c22a38f4d79ec8c1b93c8c745d89c0f8623
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2026-03-08 20:25:54 +0200

    wip

--- mtest.cpp
+++ mtest.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Copyright © 2018, 2024-2026 Ivan Pizhenko. All rights reserved.
+// Copyright (c) 2018, 2024-2026 Ivan Pizhenko. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
