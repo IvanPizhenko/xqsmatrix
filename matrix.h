f3e14688dbb8f93ca53cd3de814f02aa95736d89
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2026-03-18 23:26:26 +0200

    wip

--- matrix.h
+++ matrix.h
@@ -1483,9 +1483,9 @@ public:
       size_type pos,
       size_type count = 1,
       const_reference v = T())
-      {
-        // TODO: implement
-      }
+  {
+    // TODO: implement
+  }
 
   // Remove "count" columns at postion "pos"
   void remove_columns(size_type pos, size_type count = 1)
